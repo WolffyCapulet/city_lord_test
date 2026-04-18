972bbe7a9f19c058d6884004f7c8919116598582
WolffyCapulet <ms0342264@gmail.com>
2026-04-18 16:43:25 +0800

    Update bindEvents.js

--- js/app/bindEvents.js
+++ js/app/bindEvents.js
@@ -7,12 +7,28 @@ export function bindEvents({
   onCancelAction,
   onClearActionQueue,
   onSetLogFilter,
-  onSetMainPage
+  onSetMainPage,
+  onClaimTax,
+  onPayDebt,
+  onRecruitWorker,
+  onOpenSeedSelect,
+  onPlant,
+  onToggleFarmerSeedMode,
+  onToggleFarmerAutoFertilize
 }) {
   document.getElementById("restBtn")?.addEventListener("click", () => onRest?.());
   document.getElementById("eatBestBtn")?.addEventListener("click", () => onEatBest?.());
   document.getElementById("saveBtn")?.addEventListener("click", () => onSave?.());
   document.getElementById("loadBtn")?.addEventListener("click", () => onLoad?.());
+
+  document.getElementById("claimTaxBtn")?.addEventListener("click", () => onClaimTax?.());
+  document.getElementById("payDebtBtn")?.addEventListener("click", () => onPayDebt?.());
+  document.getElementById("recruitBtn")?.addEventListener("click", () => onRecruitWorker?.());
+
+  document.getElementById("seedSelectBtn")?.addEventListener("click", () => onOpenSeedSelect?.());
+  document.getElementById("plantBtn")?.addEventListener("click", () => onPlant?.());
+  document.getElementById("farmerSeedBtn")?.addEventListener("click", () => onToggleFarmerSeedMode?.());
+  document.getElementById("farmerAutoFertilizeBtn")?.addEventListener("click", () => onToggleFarmerAutoFertilize?.());
 
   document.getElementById("cancelActionBtn")?.addEventListener("click", () => onCancelAction?.());
   document.getElementById("clearActionQueueBtn")?.addEventListener("click", () => onClearActionQueue?.());
